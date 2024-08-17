a2a6f434aee4373bca160417d279c4fcab99b341
Joyce Mwangi <105669036+JOYCEmwangi8880@users.noreply.github.com>
2024-08-17 09:21:35 +0000

    feature-products-data

---
+++ src/app/data/products.tsx
@@ -0,0 +1,84 @@
+import Image from "next/image"
+const products = [
+    {
+        
+        img: '/cta-banner.jpg',
+        title: 'Women\'s Shirt',
+        price: 2600,
+        description: 'cotton',
+        rating: 4,
+        category: 'Clothing',
+        brand: 'prada'
+     },
+     {
+        
+        img: '/jacket-1.jpg',
+        title: 'Women\'s Shirt',
+        price: 2600,
+        description: 'cotton',
+        rating: 4,
+        category: 'Clothing',
+        brand: 'prada'
+     },
+     {
+        
+        img: '/jacket-2.jpg',
+        title: 'Women\'s Shirt',
+        price: 2600,
+        description: 'cotton',
+        rating: 4,
+        category: 'Clothing',
+        brand: 'prada'
+     },
+     {
+        
+        img: '/shirt-1.jpg',
+        title: 'Women\'s Shirt',
+        price: 2600,
+        description: 'cotton',
+        rating: 4,
+        category: 'Clothing',
+         brand: 'prada'
+     },
+     {
+        
+        img: '/patty-wear-1.jpg',
+        title: 'Women\'s Shirt',
+        price: 2600,
+        description: 'cotton-wear',
+        rating: 4,
+        category: 'Shoes',
+        brand: 'prada'
+     },
+     {
+        
+        img: '/watch-1.jpg',
+        title: 'Women\'s Shirt',
+        price: 2600,
+        description: 'leather pouch',
+        rating: 4,
+        category: 'Accesories',
+        brand: 'prada'
+     },
+     {
+        
+        img: '/watch-2.jpg',
+        title: 'Women\'s Shirt',
+        price: 2600,
+        description: 'leather pouch',
+        rating: 4,
+        category: 'Accesories',
+        brand: 'prada'
+     },
+     {
+        
+        img: '/skirt-1.jpg',
+        title: 'Women\'s Skirt',
+        price: 2600,
+        description: 'jeans skirt',
+        rating: 4,
+        category: 'Clothing',
+        brand: 'prada'
+     }
+
+]
