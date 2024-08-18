c5eed9f9dfb97f509ff3c0984dfe245ae18b97bc
Joyce Mwangi <105669036+JOYCEmwangi8880@users.noreply.github.com>
2024-08-18 20:00:59 +0000

    new-products-page-route

---
+++ src/app/new-products/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NewProducts from '@/components/NewProducts'
+
+const page = () => {
+  return (
+    <main>
+      < NewProducts />
+    </main>
+  )
+}
+
+export default page
