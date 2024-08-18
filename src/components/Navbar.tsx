9eeac5dec4620db2785a191b9864a629e743299d
Joyce Mwangi <105669036+JOYCEmwangi8880@users.noreply.github.com>
2024-08-18 20:02:39 +0000

    feature-changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,26 @@
-import React from 'react'
+import React from 'react';
 import Link from 'next/link';
 
 const Navbar = () => {
-  return <div className='hidden lg:block'>
-    <div className='container'>
+  return (
+    <div className='hidden lg:block'>
+      <div className='container'>
         <div className='flex w-fit gap-10 mx-auto font-medium py-4 text-black'>
-
-
-        <Link className="navbar_link realtive" href="#">Home </Link>
-        <Link className="navbar_link realtive" href="#">CATEGORIES </Link>
-        <Link className="navbar_link realtive" href="#">MEN'S </Link>
-        <Link className="navbar_link realtive" href="#">WOMEN'S </Link>
-        <Link className="navbar_link realtive" href="#">JEWELLRY </Link>
-        <Link className="navbar_link realtive" href="#">PERFUME </Link>
-        <Link className="navbar_link realtive" href="#">BLOG </Link>
-        <Link className="navbar_link realtive" href="#">HOT OFFERS </Link>
+          <Link className="navbar_link relative" href="/">Home</Link>
+          <Link className="navbar_link relative" href="/categories">CATEGORIES</Link>
+          <Link className="navbar_link relative" href="/mens">MEN'S</Link>
+          <Link className="navbar_link relative" href="/womens">WOMEN'S</Link>
+          <Link className="navbar_link relative" href="/jewellery">JEWELLERY</Link>
+          <Link className="navbar_link relative" href="/perfume">PERFUME</Link>
+          <Link className="navbar_link relative" href="/blog">BLOG</Link>
+          <Link className="navbar_link relative" href="/hot-offers">HOT OFFERS</Link>
+          <Link className="navbar_link relative" href="/new-products">New Products</Link>
+          <Link className="navbar_link relative" href="/testimonials">Testimonials</Link>
+          <Link className="navbar_link relative" href="/contactus">Contact Us</Link>
         </div>
-        
+      </div>
     </div>
+  );
+};
 
-
-  </div>
-  
-}
-
-export default Navbar
+export default Navbar;
