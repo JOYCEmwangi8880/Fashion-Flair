5c74f063bf13c95347778e2149129e845e6dcfd1
Joyce Mwangi <105669036+JOYCEmwangi8880@users.noreply.github.com>
2024-08-18 20:01:25 +0000

    testimonials-page-route

---
+++ src/app/testimonials/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Testimonials from '@/components/Testimonials'
+
+const page = () => {
+  return (
+   <main>
+    < Testimonials />
+   </main>
+  )
+}
+
+export default page
