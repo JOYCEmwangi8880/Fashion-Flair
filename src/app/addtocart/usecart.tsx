db8d29664a7fa956749f4a45a1b2c1a5c529e9c5
Joyce Mwangi <105669036+JOYCEmwangi8880@users.noreply.github.com>
2024-09-03 09:31:52 +0000

    addtocart-comp

---
+++ src/app/addtocart/usecart.tsx
@@ -0,0 +1,33 @@
+"use client"
+import { useState, useContext, createContext } from 'react';
+
+
+ export const CartContext = createContext();
+
+
+export function useCart() {
+  return useContext(CartContext);
+}
+
+
+export function CartProvider({ children }) {
+  const [cartItems, setCartItems] = useState([]);
+
+  
+  const addToCart = (item) => {
+    setCartItems((prevItems) => [...prevItems, item]);
+  };
+
+  
+  const removeFromCart = (itemId) => {
+    setCartItems((prevItems) =>
+      prevItems.filter((item) => item.id !== itemId)
+    );
+  };
+
+  return (
+    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
