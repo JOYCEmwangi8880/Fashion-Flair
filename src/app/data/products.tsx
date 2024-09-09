7b96f35d334d24092de088b827a94f59f8481e47
Joyce Mwangi <105669036+JOYCEmwangi8880@users.noreply.github.com>
2024-09-09 11:14:06 +0000

    productsdata

--- src/app/data/products.tsx
+++ src/app/data/products.tsx
@@ -119,4 +119,14 @@ export const productsData = [
     category: "Clothing",
     brand: "prada",
   },
+  {
+    id: 13,
+    img: "/short-1.jpg",
+    title: "Women's Skirt",
+    price: 2300,
+    description: "jeans skirt",
+    rating: 4,
+    category: "Clothing",
+    brand: "prada",
+  },
 ];
